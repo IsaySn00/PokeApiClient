cce7674f30d82fe487457e96dbb1f1cc8246c9f3
Alien 1 <Alien 1@192.167.0.77>
2025-12-30 10:17:19 -0600

    Primer commit pokeAPI cliente

---
+++ src/main/java/com/digis01/PokeApiClient/PokeApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.digis01.PokeApiClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digis01/PokeApiClient/PokeApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digis01.PokeApiClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
